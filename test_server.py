09760b3e1106eae71e72a6b0c0ed713a014afee9
tremlab <tremlab@icloud.com>
2017-05-13 21:39:13 +0000

    testing server on heroku

---
+++ test_server.py
@@ -0,0 +1,14 @@
+from flask import Flask, request
+from random import choice
+
+COMPLIMENTS = ["smart", "clever", "tenacious", "awesome", "Pythonic"]
+
+@app.route('/')
+def offer_greeting():
+    """Greet user."""
+
+    player = request.args.get("person")  # ... what the user typed!
+    nice_thing = choice(COMPLIMENTS)
+
+    return "<html><body>Hi, %s. I think you're %s!</body></html>" % (
+        player, nice_thing)
