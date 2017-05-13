1e1ef0e370669bef7b4c552ef76b4d93561be957
agerista <amandacrawfordcodes@gmail.com>
2017-05-13 22:38:05 +0000

    simplify for testing

--- test_server.py
+++ test_server.py
@@ -1,14 +1,14 @@
-from flask import Flask, request
-from random import choice
+# from flask import Flask, request
+# from random import choice
 
-COMPLIMENTS = ["smart", "clever", "tenacious", "awesome", "Pythonic"]
+# COMPLIMENTS = ["smart", "clever", "tenacious", "awesome", "Pythonic"]
 
-@app.route('/')
-def offer_greeting():
-    """Greet user."""
+# @app.route('/')
+# def offer_greeting():
+#     """Greet user."""
 
-    player = request.args.get("person")  # ... what the user typed!
-    nice_thing = choice(COMPLIMENTS)
+#     player = request.args.get("person")  # ... what the user typed!
+#     nice_thing = choice(COMPLIMENTS)
 
-    return "<html><body>Hi, %s. I think you're %s!</body></html>" % (
-        player, nice_thing)
+#     return "<html><body>Hi, %s. I think you're %s!</body></html>" % (
+#         player, nice_thing)
