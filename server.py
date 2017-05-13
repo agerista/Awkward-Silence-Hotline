3a045dd9ebc7446db2e5eda09ec8435df8f5d700
tremlab <tremlab@icloud.com>
2017-05-13 22:05:50 +0000

    server

--- server.py
+++ server.py
@@ -1,17 +1,12 @@
 from flask import Flask, request
 from random import choice
 
-COMPLIMENTS = ["smart", "clever", "tenacious", "awesome", "Pythonic"]
 
 @app.route('/')
 def offer_greeting():
     """Greet user."""
 
-    player = "you"  # ... what the user typed!
-    nice_thing = choice(COMPLIMENTS)
-
-    return "<html><body>Hi, %s. I think you're %s!</body></html>" % (
-        player, nice_thing)
+    return "<html><body>awkward....</body></html>" 
 
 if __name__ == "__main__":
     DEBUG = "NO_DEBUG" not in os.environ
