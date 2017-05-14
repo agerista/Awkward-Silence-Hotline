38ef2562efafec21d186881135acfff1caee91db
agerista <amandacrawfordcodes@gmail.com>
2017-05-14 16:06:37 +0000

    added possible website photos

--- server.py
+++ server.py
@@ -17,8 +17,8 @@ callers = {
 
 
 @app.route("/", methods=['GET', 'POST'])
-def hello_monkey():
-    """Respond to incoming requests."""
+def awkward_silence_hotline():
+    """awkward silence hotline phoneline"""
     # resp = VoiceResponse()
     # resp.say("Uhhhhhh")
 
@@ -27,7 +27,7 @@ def hello_monkey():
     if from_number in callers:
         caller = callers[from_number]
     else:
-        caller = "Monkey"
+        caller = "Stupid Hacker"
 
     resp = VoiceResponse()
     # Greet the caller by name
@@ -37,8 +37,10 @@ def hello_monkey():
 
     # Gather digits.
     with resp.gather(numDigits=1, action="/handle-key", method="POST") as g:
-        g.say("""To speak to a real monkey, press 1. 
-                 Press 2 to record your own monkey howl.
+        g.say("""I am the Lord Mayor of Awkward Town, press 1.
+                 Press 2 for I have foot in mouth disease.
+                 Press 3 for my boyfriend just broke up with me and I'm pregnant.
+                 Press 4 for I just can't.
                  Press any other key to start over.""")
 
     return str(resp)
@@ -61,10 +63,10 @@ def handle_key():
     elif digit_pressed == "2":
         resp = VoiceResponse()
         resp.say("Record your monkey howl after the tone.")
-        resp.record(maxLength="30", action="/handle-recording")
+        resp.record(maxLength="30", action="/handle-recording", method="POST")
         return str(resp)
 
-    # If the caller pressed anything but 1, redirect them to the homepage.
+    # If the caller pressed anything but 1 or 2, redirect them to the homepage.
     else:
         return redirect("/")
 
