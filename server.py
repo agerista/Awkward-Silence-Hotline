4c5a438ccdeefeb3522731b2d26ad429f3364e86
agerista <amandacrawfordcodes@gmail.com>
2017-05-14 01:25:59 +0000

    testing voice

--- server.py
+++ server.py
@@ -7,6 +7,7 @@ from twilio.twiml.voice_response import VoiceResponse
 
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = os.environ.get("FLASK_SECRET_KEY", "s0Then!stO0dth34ean9a11iw4n7edto9ow4s8ur$7!ntOfL*me5")
 fake = Factory.create()
 alphanumeric_only = re.compile('[\W_]+')
 phone_pattern = re.compile(r"^[\d\+\-\(\) ]+$")
@@ -18,6 +19,17 @@ def index():
 
     return render_template('index.html')
 
+
+@app.route("/voice", methods=['GET', 'POST'])
+def voice():
+    """Respond to incoming phone calls with a 'Hello world' message"""
+    # Start our TwiML response
+    resp = VoiceResponse()
+
+    # Read a message aloud to the caller
+    resp.say("hello world!", voice='alice')
+
+    return str(resp)
 ################################################################################
 if __name__ == "__main__":
 
