37d550e006e20febe61d563b4964419d7cd93194
agerista <amandacrawfordcodes@gmail.com>
2017-05-14 00:39:55 +0000

    rebase

--- server.py
+++ server.py
@@ -11,54 +11,12 @@ fake = Factory.create()
 alphanumeric_only = re.compile('[\W_]+')
 phone_pattern = re.compile(r"^[\d\+\-\(\) ]+$")
 
-# Normally, if you use an undefined variable in Jinja2, it fails
-# silently. This is horrible. Fix this so that, instead, it raises an
-# error.
-# app.jinja_env.undefined = StrictUndefined
-
 
 @app.route('/')
 def index():
-    return app.send_static_file('index.html')
-    # app.send_static_file('index.html')
+    """Shows the homepage"""
 
-
-@app.route('/token.jsonify', methods=['GET'])
-def token():
-    # get credentials for environment variables
-    account_sid = os.environ['TWILIO_ACCOUNT_SID']
-    auth_token = os.environ['TWILIO_AUTH_TOKEN']
-    application_sid = os.environ['TWILIO_TWIML_APP_SID']
-
-    # Generate a random user name
-    identity = alphanumeric_only.sub('', fake.user_name())
-
-    # Create a Capability Token
-    capability = ClientCapabilityToken(account_sid, auth_token)
-    capability.allow_client_outgoing(application_sid)
-    capability.allow_client_incoming(identity)
-    token = capability.to_jwt()
-
-    # Return token info as JSON
-    return jsonify(identity=identity, token=token)
-
-
-@app.route("/voice", methods=['POST'])
-def voice():
-    resp = VoiceResponse()
-    if "To" in request.form and request.form["To"] != '':
-        dial = resp.dial(callerId=os.environ['TWILIO_CALLER_ID'])
-        # wrap the phone number or client name in the appropriate TwiML verb
-        # by checking if the number given has only digits and format symbols
-        if phone_pattern.match(request.form["To"]):
-            dial.number(request.form["To"])
-        else:
-            dial.client(request.form["To"])
-    else:
-        resp.say("Thanks for calling!")
-
-    return Response(str(resp), mimetype='text/xml')
-
+    return render_template('index.html')
 
 ################################################################################
 if __name__ == "__main__":
